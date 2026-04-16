fa289e89233eea497df8ce4ecfe4a99afef86e35
Steven Phan <stevephan08@gmail.com>
2026-04-16 16:14:46 -0400

    fix: remove dead Spot UV Stickers link from navbar

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -13,7 +13,6 @@ const navLinks = [
     href: '/shop',
     children: [
       { label: 'Stickers & Labels', href: '/shop/stickers' },
-      { label: 'Spot UV Stickers', href: '/shop/spot-uv-stickers' },
       { label: 'Roll Labels', href: '/shop/roll-labels' },
     ],
   },
