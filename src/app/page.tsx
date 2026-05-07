22660593edd5adceac0ea5d4c3dd783fdc68ecf7
Steven Phan <stevephan08@gmail.com>
2026-05-07 16:37:27 -0400

    Move services above testimonials

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,8 +17,8 @@ export default function HomePage() {
         <TrustBar />
         <ShopAllSection />
         <PortfolioStrip />
-        <Testimonials />
         <ServicesOverview />
+        <Testimonials />
         <CTABanner />
       </main>
       <Footer />
